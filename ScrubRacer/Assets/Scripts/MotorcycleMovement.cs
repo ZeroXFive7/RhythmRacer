5fc3621a739b5d9b9cafcc48e6ec38574482b768
Josh Wehrly <josh.wehrly@gmail.com>
2014-05-31 18:56:58 -0700

    Added record model

---
+++ ScrubRacer/Assets/Scripts/MotorcycleMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotorcycleMovement : MonoBehaviour
+{
+    public float AccelerationRate;
+    public float Drag;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        var forward = this.transform.forward;
+        Debug.DrawRay(this.transform.position, forward * 10.0f, Color.red);
+
+        var acceleration = forward * GetAcceleration();
+        this.rigidbody.AddForce(acceleration, ForceMode.Acceleration);
+	}
+
+    private float GetAcceleration()
+    {
+        var accelerationInput = Input.GetAxis("Forward") - Input.GetAxis("Reverse");
+
+        return this.AccelerationRate * accelerationInput;
+    }
+}
