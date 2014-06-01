120de3d5ec81a08e664063c3bafed4e5c343a316
Josh Wehrly <josh.wehrly@gmail.com>
2014-05-31 20:32:05 -0700

    Added jump to record

--- ScrubRacer/Assets/Scripts/MotorcycleMovement.cs
+++ ScrubRacer/Assets/Scripts/MotorcycleMovement.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class MotorcycleMovement : MonoBehaviour
 {
     public float AccelerationRate;
-    public float Drag;
 
 	// Use this for initialization
 	void Start ()
@@ -15,11 +14,12 @@ public class MotorcycleMovement : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        var forward = this.transform.forward;
+        var forward = GetForward();
         Debug.DrawRay(this.transform.position, forward * 10.0f, Color.red);
 
         var acceleration = forward * GetAcceleration();
-        this.rigidbody.AddForce(acceleration, ForceMode.Acceleration);
+        this.rigidbody.AddForce(acceleration * this.rigidbody.mass);
+        Debug.Log("Velocity: " + rigidbody.velocity);
 	}
 
     private float GetAcceleration()
@@ -27,5 +27,33 @@ public class MotorcycleMovement : MonoBehaviour
         var accelerationInput = Input.GetAxis("Forward") - Input.GetAxis("Reverse");
 
         return this.AccelerationRate * accelerationInput;
+    }
+
+    private Vector3 GetForward()
+    {
+        return ProjectVecToSurface(this.transform.forward);
+    }
+
+    private Vector3 ProjectVecToSurface(Vector3 forward)
+    {
+        var ray = new Ray(this.transform.position, Vector3.down);
+        RaycastHit hit;
+
+        float colliderHeight = this.collider.transform.localScale.y * this.collider.bounds.size.y;
+        if (!Physics.Raycast(ray, out hit, (colliderHeight / 2.0f * 1.05f)))
+        {
+            Debug.LogWarning("Raycast failed.");
+            return forward;
+        }
+
+        return ProjectVecToPlane(forward, hit.normal);
+    }
+
+    private Vector3 ProjectVecToPlane(Vector3 vector, Vector3 planeNormal)
+    {
+        var planeNormalNorm = planeNormal.normalized;
+        var vectorNorm = vector.normalized;
+
+        return (vectorNorm - Vector3.Dot(vectorNorm, planeNormalNorm) * planeNormalNorm).normalized;
     }
 }
